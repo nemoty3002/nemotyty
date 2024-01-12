02cce933b7465c2c05c33c9a5b84448bc39be992
nemoty3002 <131091830+nemoty3002@users.noreply.github.com>
2024-01-12 15:09:26 +0900

    Update xe45.js

--- xe45.js
+++ xe45.js
@@ -1,1 +1,1 @@
-function sayhello45() { alert('『キングダム ハーツ』（KINGDOM HEARTS、略称: KH）は、スクウェア（現：スクウェア・エニックス）より2002年3月28日に発売されたPlayStation 2用ゲームソフト。'); }
+function sayhello45() { alert('『キングダム ハーツ』は、スクウェア・エニックスより2002年3月28日に発売されたゲーム。'); }
